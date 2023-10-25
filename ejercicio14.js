ea29673a84f4f4593bfc3c2811d5efa9cdd54081
Sergiioog <sergiogargor9@gmail.com>
2023-10-25 19:44:55 +0200

    Ejercicios

---
+++ ejercicio14.js
@@ -0,0 +1,3 @@
+//14. Añadir al ejercicio anterior que nos diga por cual de 
+//los cuatro es divisible (hayque decir todos por los que es divisible)
+
---
+++ ejercicio15.js
@@ -0,0 +1,7 @@
+//Escribir un programa que escriba en pantalla los divisores de un número dado
+
+var numero = prompt("Introduzca su número: ");
+
+for (let i = numero; i <= numero/2 ; i++) {
+    console.log(numero);
+}
